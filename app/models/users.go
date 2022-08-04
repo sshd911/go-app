2f22ad699200eee3a5390841f9171244b843e181
Sho Maeda <syou20020911@gmail.com>
2022-08-05 08:27:43 +0900

    delete user

--- app/models/users.go
+++ app/models/users.go
@@ -58,3 +58,12 @@ func (u *User) UpdateUser() (err error) {
 	}
 	return err
 }
+
+func (u *User) DeleteUser() (err error) {
+	cmd := `DELETE FROM users WHERE id = ?`
+	_, err = Db.Exec(cmd, u.ID)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	return err
+}
