ac1cbc18dfa9959f9ee90198f23d7d9589241cae
Sho Maeda <syou20020911@gmail.com>
2022-08-05 08:59:31 +0900

    create todos table

--- main.go
+++ main.go
@@ -7,22 +7,13 @@ import (
 
 func main(){
 	fmt.Println(models.Db)
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
-
-	// log.Println("test")
-
-	// fmt.Println(models.Db)
-
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@example.com"
-	// u.Password = "testtest"
-	// fmt.Println(u)
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.Password = "testtest"
+	fmt.Println(u)
 	
-	// u.CreateUser()
+	u.CreateUser()
 
 	// u, _ := models.GetUser(1)
 	// fmt.Println(u)
@@ -36,4 +27,6 @@ func main(){
 	// u.DeleteUser()
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("first todo")
 }
