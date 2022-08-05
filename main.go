6164caf1ffad1ee29380d1e7e6ce050302db7fe5
Sho Maeda <syou20020911@gmail.com>
2022-08-05 09:05:55 +0900

    get todo

--- main.go
+++ main.go
@@ -7,13 +7,15 @@ import (
 
 func main(){
 	fmt.Println(models.Db)
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@example.com"
-	u.Password = "testtest"
-	fmt.Println(u)
-	
-	u.CreateUser()
+	/*
+		u := &models.User{}
+		u.Name = "test"
+		u.Email = "test@example.com"
+		u.Password = "testtest"
+		fmt.Println(u)
+		
+		u.CreateUser()
+	*/
 
 	// u, _ := models.GetUser(1)
 	// fmt.Println(u)
@@ -27,6 +29,8 @@ func main(){
 	// u.DeleteUser()
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
-	user, _ := models.GetUser(2)
-	user.CreateTodo("first todo")
+	// user, _ := models.GetUser(2)
+	// user.CreateTodo("first todo")
+	t, _ := models.GetTodo(1)
+	fmt.Println(t)
 }
